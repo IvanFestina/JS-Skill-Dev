223bd3aaf657edf4b5c366447bf45e97e88e3445
IvanFestina <ivanfestina@gmail.com>
2022-01-01 20:09:32 +0300

    01.01.22

--- ts-react-kabzda/src/stories/assets/input.stories.tsx
+++ ts-react-kabzda/src/stories/assets/input.stories.tsx
@@ -1,48 +1,58 @@
-import React from 'react';
-import './button.css';
+import React, {ChangeEvent, useRef, useState} from 'react';
+import {Action} from "@storybook/addon-actions/dist/ts3.9/components/ActionLogger/style";
+import {action} from "@storybook/addon-actions";
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+export default {
+    title: 'UncontrolledInput',
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
+export const UncontrolledInput = () => <input/>
+export const TrackValueOfUncontrolledInput = () => {
+    const [value, setValue] = useState('')
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.currentTarget.value)
+    }
+    return <> <input onChange={onChange}/> - {value} </>
+}
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState('');
+    const inputRef = useRef<HTMLInputElement>(null)
+    const save = () => {
+        const el = inputRef.current as HTMLInputElement;
+        setValue(el.value);
+    }
+
+    return <> <input ref={inputRef}/>
+        <button onClick={save}>save</button>
+        - {value}
+    </>
+}
+export const ControlledInput = () => {
+    const [parentValue, setParrentValue] = useState<string>('')
+    const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParrentValue(e.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChangeInputHandler}/>
+}
+export const ControlledCheckbox = () => {
+    const [parentValue, setParrentValue] = useState<boolean>(true)
+    const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParrentValue(e.currentTarget.checked)
+    }
+    return <input type={'checkbox'} checked={parentValue} onChange={onChangeInputHandler}/>
+}
+export const ControlledSelect = () => {
+    const [parentValue, setParrentValue] = useState<string | undefined>('')
+    const onChangeSelectHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParrentValue(e.currentTarget.value)
+    }
+    return <select value={parentValue} onChange={onChangeSelectHandler}>
+        <option>none</option>
+        <option value={1}>Minsk</option>
+        <option value={2}>Moscow</option>
+        <option value={3}>Kiev</option>
+    </select>
+}
+
+export const ControlledInputWithFixedValue = () => <input value={'it-incubator.by'}/>
+
