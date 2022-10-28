bb0052ab09272e2ded5bbb4be3791d9f7555de40
IvanFestina <ivanfestina@gmail.com>
2022-10-28 23:26:58 +0300

    tests and more

---
+++ JS Algorithms/7_Dijkstra's_algorithm.js
@@ -0,0 +1,13 @@
+// Поиск кратчайшего пути в графе
+
+const graph = {}
+graph.a = {b: 2, c: 1}
+graph.b = {f: 7}
+graph.c = {d: 5, e: 2}
+graph.d = {f: 2}
+graph.e = {f: 1}
+graph.f = {g: 1}
+graph.g = {}
+
+
+console.log(shortPath(graph, 'a', 'g'));
