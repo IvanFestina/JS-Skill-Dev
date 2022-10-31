2852a684220149d9b7968d3486a75c3499a4d470
IvanFestina <ivanfestina@gmail.com>
2022-10-31 23:46:38 +0300

    tests and more

--- JS Algorithms/8_tree_search.js
+++ JS Algorithms/8_tree_search.js
@@ -38,32 +38,61 @@ const tree = [
     }
 ]
 
+// const recursive = (tree) => {
+//     let sum = 0;
+//     tree.forEach(node => {
+//         sum += node.v
+//         if (!node.c) return node.v
+//         sum += recursive(node.c)
+//     })
+//     return sum
+// }
 const recursive = (tree) => {
     let sum = 0;
     tree.forEach(node => {
         sum += node.v
-        if (!node.c) return node.v
+        if(!node.c) {
+            return node.v
+        }
         sum += recursive(node.c)
     })
     return sum
 }
 
+
+// const iteration = (tree) => {
+//     if (!tree.length) {
+//         return 0
+//     }
+//     let sum = 0;
+//     let stack = [];
+//     tree.forEach(node => stack.push(node))
+//     while (stack.length) {
+//         const node = stack.pop()
+//         sum +=node.v;
+//         if(node.c) {
+//             node.c.forEach(child => stack.push(child))
+//         }
+//     }
+//     return sum;
+// }
 const iteration = (tree) => {
-    if (!tree.length) {
-        return 0
-    }
     let sum = 0;
     let stack = [];
-    tree.forEach(node => stack.push(node))
-    while (stack.length) {
-        const node = stack.pop()
-        sum +=node.v;
-        if(node.c) {
-            node.c.forEach(child => stack.push(child))
+    if(!tree.length) return 0;
+    tree.forEach(node => {
+        stack.push(node)
+        while(stack.length) {
+            const node = stack.pop();
+            sum += node.v
+            if(node.c) {
+                node.c.forEach(child => {
+                stack.push(child)
+                })
+            }
         }
-    }
-    return sum;
+    })
+    return sum
 }
-
 console.log(recursive(tree))
 console.log(iteration(tree))
